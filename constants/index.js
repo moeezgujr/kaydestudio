2a0bdab28017a8217d082d89159899bc4501885f
moeezgujr <73531265+moeezgujr@users.noreply.github.com>
2021-06-23 23:31:52 +0500

    hooks

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+import nowTheme from './Theme';
+import articles from './articles';
+import Images from './Images';
+import tabs from './tabs';
+import utils from './utils';
+import jobs from './jobs';
+
+export { articles, nowTheme, Images, tabs, utils,jobs };
---
+++ constants/jobs.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      title: 'Play Guitar at Easter',
+      image: require("../assets/imgs/download.jpg"),
+      cta: 'View Job',
+      price:'400$',
+      horizontal: true
+    },
+    {
+        title: 'Play Guitar at Easter',
+        image: require("../assets/imgs/downloadone.jpg"),
+      cta: 'View Job',
+      price:'500$',
+      horizontal: true
+    },
+    {
+      title: 'Write Scripts for piano',
+      image: require("../assets/imgs/downloadscript.png"),
+      cta: 'View Job',
+      price:'500$',
+      horizontal: true
+
+    },
+   
+  ];
+  
